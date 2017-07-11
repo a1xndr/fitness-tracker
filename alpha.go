3eb3fa7252de18ccef70ab2dcf16aec06bfd786b
Alexander Oleinik <alxndr@bu.edu>
2017-07-10 23:23:32 -0400

    added basic /workout/ template

--- alpha.go
+++ alpha.go
@@ -32,13 +32,11 @@ func Max(x, y int) int {
 	}
 	return y
 }
-func (w *Workout) SaveWorkout() error {
-	w.Sets = append(w.Sets, Set{Exercise: "squat", Reps: 5, Weight: 22})
-	w.Sets = append(w.Sets, Set{Exercise: "bench", Reps: 5, Weight: 225})
-	w.Sets = append(w.Sets, Set{Exercise: "deadlift", Reps: 10, Weight: 225})
-	filename := fmt.Sprintf("%04d-%02d-%02d.txt", w.Time.Year(), w.Time.Month(), w.Time.Day())
-	title := fmt.Sprintf("Workout on %04d-%02d-%02d\n", w.Time.Year(), w.Time.Month(), w.Time.Day())
-	title += strings.Repeat("=", len(title)-1) + "\n\n"
+
+func (w *Workout) FormattedDate() string {
+	return fmt.Sprintf("%04d-%02d-%02d\n", w.Time.Year(), w.Time.Month(), w.Time.Day())
+}
+func (w *Workout) FormatAsMd() string {
 	var wlog string
 	for i, s := range w.Sets {
 		wlog += "* Set: " + fmt.Sprint(i) + "\n"
@@ -46,6 +44,60 @@ func (w *Workout) SaveWorkout() error {
 		wlog += "    * Reps:" + fmt.Sprint(s.Reps) + "\n"
 		wlog += "    * Weight:" + fmt.Sprint(s.Weight) + "\n"
 	}
+	return wlog
+}
+
+func (w *Workout) FormatAsAsciiTable() string {
+	exc_max_len := len("Exercise ")
+	rep_max_len := len("Reps ")
+	wgt_max_len := len("Weight ")
+	var Exercises []string
+	var Reps []string
+	var Weight []string
+	for _, s := range w.Sets {
+		Exercises = append(Exercises, fmt.Sprint(s.Exercise))
+		Reps = append(Reps, fmt.Sprint(s.Reps))
+		Weight = append(Weight, fmt.Sprint(s.Weight))
+		if len(Exercises[len(Exercises)-1]) > exc_max_len {
+			exc_max_len = len(Exercises[len(Exercises)-1])
+		}
+		if len(Reps[len(Exercises)-1]) > exc_max_len {
+			rep_max_len = len(Reps[len(Exercises)-1])
+		}
+		if len(Weight[len(Exercises)-1]) > exc_max_len {
+			wgt_max_len = len(Weight[len(Exercises)-1])
+		}
+	}
+	Table := "| Exercise "
+	Table = Table + strings.Repeat(" ", Max(0, exc_max_len-len("Exercise ")))
+	Table = Table + " | Reps "
+	Table = Table + strings.Repeat(" ", Max(0, rep_max_len-len("Reps ")))
+	Table = Table + " | Weight "
+	Table = Table + strings.Repeat(" ", Max(0, wgt_max_len-len("Weight ")))
+	Table = Table + " |\n"
+	for i, _ := range Exercises {
+		str := fmt.Sprint(Exercises[i])
+		Table = Table + "| " + str
+		Table = Table + strings.Repeat(" ", Max(0, exc_max_len-len(str)))
+		str = fmt.Sprint(Reps[i])
+		Table = Table + " | " + str
+		Table = Table + strings.Repeat(" ", Max(0, rep_max_len-len(str)))
+		str = fmt.Sprint(Weight[i])
+		Table = Table + " | " + str
+		Table = Table + strings.Repeat(" ", Max(0, wgt_max_len-len(str)))
+		Table = Table + " |\n"
+	}
+	return Table
+}
+
+func (w *Workout) SaveWorkout() error {
+	w.Sets = append(w.Sets, Set{Exercise: "squat", Reps: 5, Weight: 22})
+	w.Sets = append(w.Sets, Set{Exercise: "bench", Reps: 5, Weight: 225})
+	w.Sets = append(w.Sets, Set{Exercise: "deadlift", Reps: 10, Weight: 225})
+	filename := fmt.Sprintf("%04d-%02d-%02d.txt", w.Time.Year(), w.Time.Month(), w.Time.Day())
+	title := fmt.Sprintf("Workout on %04d-%02d-%02d\n", w.Time.Year(), w.Time.Month(), w.Time.Day())
+	title += strings.Repeat("=", len(title)-1) + "\n\n"
+	wlog := w.FormatAsMd()
 	fmt.Println(wlog)
 	return ioutil.WriteFile(filename, []byte(title+wlog), 0600)
 }
@@ -53,12 +105,15 @@ func (w *Workout) SaveWorkout() error {
 func LoadWorkout(date string) (*Workout, error) {
 	//filename := fmt.Sprintf("%04d-%02d-%02d.txt", date.Year(), .Month(), w.Time.Day())
 	filename := date + ".txt"
-	f, _ := os.Open(filename)
+	f, err := os.Open(filename)
 	defer f.Close()
 
 	layout := "2006-01-02"
 	d, _ := time.Parse(layout, date)
 	w := Workout{Time: d}
+	if err != nil {
+		return &w, nil
+	}
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		matchedSet, _ := regexp.MatchString("Set:", scanner.Text())
@@ -80,13 +135,13 @@ func LoadWorkout(date string) (*Workout, error) {
 
 func WorkoutTaskFunc(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/workout/"):]
-	if id == "" {
-		workout := &Workout{Time: time.Now()}
-		workout.SaveWorkout()
-	} else {
-		workout, _ := LoadWorkout(fmt.Sprintf("%04d-%02d-%02d", time.Now().Year(), time.Now().Month(), time.Now().Day()))
-		fmt.Fprintf(w, "%+v\n", workout)
+	t := time.Now()
+	if id != "" {
 	}
+	workout, _ := LoadWorkout(fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day()))
+	tmpl := template.Must(template.ParseFiles("workout.gtpl"))
+	tmpl.Execute(w, workout)
+
 }
 func ProcessWorkoutFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
