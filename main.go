6811ee257a83542960c72af7d33668be71178b59
Alexander Oleinik <alxndr@bu.edu>
2017-08-28 23:58:58 -0400

    all existing functionality clusterfucked into controllers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"alpha/controllers"
 	"alpha/web"
 	"github.com/stretchr/graceful"
 	//	"time"
@@ -9,5 +10,15 @@ import (
 func main() {
 	port := "8888"
 	s := web.NewServer()
+
+	workoutsc := controllers.ServiceController{"/workout/", controllers.WorkoutTaskFunc}
+	workoutsc.Register(s)
+
+	exercisesc := controllers.ServiceController{"/exercise/", controllers.ExerciseTaskFunc}
+	exercisesc.Register(s)
+
+	dashboardsc := controllers.ServiceController{"/dashboard/", controllers.DashboardTaskFunc}
+	dashboardsc.Register(s)
+
 	graceful.Run(":"+port, 0, s)
 }
