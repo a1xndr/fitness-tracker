bbeffcb0593cdf6825162057032149f1698b3188
Alexander Oleinik <alxndr@bu.edu>
2017-08-30 23:20:39 -0400

    a little rearanging of the http handlers

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"alpha/controllers"
 	"alpha/web"
 	"github.com/stretchr/graceful"
-	"net/http"
 )
 
 func main() {
@@ -19,16 +18,6 @@ func main() {
 
 	dashboardsc := controllers.ServiceController{"/dashboard/", controllers.DashboardTaskFunc}
 	dashboardsc.Register(s)
-
-	// css and images
-	staticsc := controllers.ServiceController{"/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))).ServeHTTP}
-	staticsc.Register(s)
-
-	// redirect GET / to dashboard
-	rootsc := controllers.ServiceController{"/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "dashboard", 301)
-	}}
-	rootsc.Register(s)
 
 	graceful.Run(":"+port, 0, s)
 }
--- web/server.go
+++ web/server.go
@@ -10,6 +10,13 @@ type Server struct {
 
 func NewServer() *Server {
 	s := Server{http.NewServeMux()}
+
+	s.HandleFunc("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))).ServeHTTP)
+
+	s.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "dashboard", 301)
+	})
+
 	return &s
 
 }
